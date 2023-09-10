134aa06df0a75f7d0e31e798f97cf04125bbe1ca
NathanPERIER <periernathan2000@gmail.com>
2023-09-10 02:25:35 +0200

    Added a data structure to hold the files' information

---
+++ src/collections/file_tree.py
@@ -0,0 +1,47 @@
+
+
+from datetime import datetime
+
+from typing import Final
+
+
+class FilesystemLeaf :
+
+    def __init__(self, filename: str, extension: str, modification: datetime) :
+        self.filename: Final[str] = filename
+        self.extensions: map[str, datetime] = { extension: modification }
+    
+    def add_extension(self, extension: str, modification: datetime) :
+        self.extensions[extension] = modification
+
+
+class FilesystemNode :
+
+    def __init__(self, name: str) :
+        self.name = name
+        self.subfolders: map[str, FilesystemNode] = {}
+        self.files: map[str, FilesystemLeaf] = {}
+        self.file_count = 0
+
+    def _add_subfolder(self, name: str) -> "FilesystemNode" :
+        self.file_count += 1
+        if name not in self.subfolders :
+            res = FilesystemNode(name)
+            self.subfolders[name] = res
+            return res
+        return self.subfolders[name]
+    
+    def _add_file(self, filename: str, extension: str, modification: datetime) :
+        self.file_count += 1
+        if filename in self.files :
+            self.files[filename].add_extension(extension, modification)
+            return
+        self.files[filename] = FilesystemLeaf(filename, extension, modification)
+
+    def push_file(self, path: str, filename: str, extension: str, modification: datetime) :
+        folders = path.split('/')
+        node = self
+        for folder in folders :
+            node = node._add_subfolder(folder)
+        node._add_file(filename, extension, modification)
+
