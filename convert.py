57d212ed1fad005e287cbff1ddc4e2dd982ca7c2
NathanPERIER <periernathan2000@gmail.com>
2024-01-14 02:55:13 +0100

    Added program options for a threshold and a default value on the `Convert-Keep` tag

--- convert.py
+++ convert.py
@@ -5,6 +5,7 @@ from collections import deque
 
 from src.options import options as prog_options
 from src.conversion import conversion
+from src.metadata.parsing import parse_keep
 
 
 def help() :
@@ -26,6 +27,25 @@ def main() :
     if len(args) > 0 and args[0] == '--no-remove' :
         prog_options.can_remove = False
         args.popleft()
+    
+    if len(args) > 1 and args[0] == '--keep-threshold' :
+        args.popleft()
+        keep_repr = args.popleft().lower()
+        if keep_repr != 'all' :
+            keep = parse_keep(keep_repr)
+            if keep == None :
+                print(f"Bad keep threshold : {keep_repr}")
+                sys.exit(1)
+            prog_options.keep_treshold = keep
+    
+    if len(args) > 1 and args[0] == '--default-keep' :
+        args.popleft()
+        keep = parse_keep(args.popleft().lower())
+        if keep == None :
+            print(f"Bad default Convert-Keep value : {keep_repr}")
+            sys.exit(1)
+        prog_options.default_keep = keep
+
     
     if len(args) != 2 :
         help()
--- src/options.py
+++ src/options.py
@@ -1,9 +1,14 @@
 
+from src.metadata.keep import ConvertKeep
+
 
 class ProgramOptions :
 
     def __init__(self) :
         self.can_remove: bool           = True
         self.dry_run:    bool           = False
+        self.default_keep:  ConvertKeep = ConvertKeep.ALWAYS
+        self.keep_treshold: ConvertKeep = ConvertKeep.lowest()
+
 
 options = ProgramOptions()
