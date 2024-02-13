7e89b7b6abfdfa8fbac4ef0269be5ab43a4ea95a
NathanPERIER <periernathan2000@gmail.com>
2024-02-13 15:22:47 +0100

    Added base for m4a tag parsing

--- src/metadata/parsing.py
+++ src/metadata/parsing.py
@@ -2,6 +2,7 @@
 import os
 import functools
 from mutagen.mp3 import MP3
+from mutagen.mp4 import MP4
 from mutagen.flac import FLAC
 from io import BytesIO
 
@@ -49,6 +50,19 @@ def metadata_mp3(path: str, leaf: FilesystemLeaf) -> RawMetadata :
     return res
 
 
+def metadata_mpeg4(path: str, leaf: FilesystemLeaf) :
+    filepath = os.path.join(path, leaf.filename())
+    res = RawMetadata()
+
+    with open(filepath, 'rb') as f :
+        tags = MP4(f).tags
+    
+    if tags is not None and 'CONVERT-KEEP' in tags :
+        res.keep = tags['CONVERT-KEEP'][0].strip()
+    
+    return res
+
+
 def metadata_flac(path: str, leaf: FilesystemLeaf) :
     filepath = os.path.join(path, leaf.filename())
     res = RawMetadata()
@@ -68,6 +82,8 @@ def read_metadata(path: str, leaf: FilesystemLeaf, default_keep: ConvertKeep) :
         raw = metadata_mp3(path, leaf)
     elif leaf.extension == 'flac' :
         raw = metadata_flac(path, leaf)
+    elif leaf.extension == 'm4a' :
+		rax = metadata_mpeg4(path, leaf)
     else :
         print(f"Unsupported extension for metadata parsing : {leaf.extension}")
     
